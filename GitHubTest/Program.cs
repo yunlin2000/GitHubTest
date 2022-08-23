c713fc1739f170c395eb31ffe9030e49802fb603
yunlin zhao <yunlin2000@163.com>
2022-08-23 20:01:26 +0800

    修改Program.cs文件

--- GitHubTest/Program.cs
+++ GitHubTest/Program.cs
@@ -10,7 +10,7 @@ namespace GitHubTest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!!!!");
+            Console.WriteLine("Hello yunlin2000!!!!");
         }
     }
 }
